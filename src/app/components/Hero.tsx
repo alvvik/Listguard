3f71adf8c32a71a7290016ee5b6d70eeac02f856
alvv <alvv2115@gmail.com>
2026-09-18 16:41:42 +0200

    feat (web): added hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import background from "@/app/public/background.jpg";
 export default function Hero() {
-  return <div></div>;
+  return (
+    <section className="hero relative min-h-screen overflow-hidden">
+      <Image
+        src={background}
+        alt=""
+        fill
+        priority
+        className="object-cover"
+        sizes="100vw"
+      />
+
+      <div className="hero-overlay absolute inset-0 bg-black/50" />
+
+      <div className="hero-content relative z-10 text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Listguard</h1>
+          <p className="mb-5">
+            Pełna integracja Discorda ze stroną WWW i landing page. Open Source,
+            bez ukrytych opłat i z pełną kontrolą nad kodem.
+          </p>
+          <div className="space-x-4">
+            <Link className="btn btn-primary" href={"/whitelist"}>
+              Aplikuj na whitelist!
+            </Link>
+            <Link className="btn" href={"https://github.com/alvvik"}>
+              Github
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
